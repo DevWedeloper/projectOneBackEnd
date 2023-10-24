adb57dfb4a631346809322da5ff3ba74f9c27c54
DevWedeloper <vicnathangabrielle@gmail.com>
2023-10-24 16:13:59 +0800

    Added isValidCharacterMiddleware to createGuild

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -43,7 +43,7 @@ const connectToDatabase = async () => {
   }
 };
 
-// connectToDatabase();
+connectToDatabase();
 app.use(express.json());
 
 app.use('/user', userRouter);
@@ -55,9 +55,9 @@ app.use('/guildStats', guildStatsRoute);
 app.use('/characterTypes', characterTypeRoute);
 app.use('/', checkNameUniquenessRoute);
 
-// app.listen(process.env.MY_PORT, () => {
-//   console.log(`Server is running on PORT ${process.env.MY_PORT}`);
-// });
+app.listen(process.env.MY_PORT, () => {
+  console.log(`Server is running on PORT ${process.env.MY_PORT}`);
+});
 // cron.schedule('0 0 * * *', refreshTokenCleanup);
 
 export const api = functions.https.onRequest((connectToDatabase(), app));

6e856b947ffaf3d53b9373ce95ab78d2ff15f540
DevWedeloper <vicnathangabrielle@gmail.com>
2023-10-27 00:18:28 +0800

    Bug Fix: Added toString() to if conditions

--- functions/src/controllers/checkIfMemberController.ts
+++ functions/src/controllers/checkIfMemberController.ts
@@ -9,10 +9,10 @@ export const checkIfMember = async (req: Request, res: Response): Promise<void |
     const { character, guild } = req.body;
     const selectedCharacter = await Character.findById(character) as ICharacter;
     const selectedGuild = await Guild.findById(guild) as IGuild;
-    if (selectedCharacter?.guild?._id === selectedGuild._id) {
+    if (selectedCharacter?.guild?._id.toString() === selectedGuild._id.toString()) {
       return res.status(200).json({ message: 'Member' });
     }
-    if (selectedCharacter?.guild?._id !== selectedGuild._id) {
+    if (selectedCharacter?.guild?._id.toString() !== selectedGuild._id.toString()) {
       return res.status(200).json({ message: 'Not member' });
     }
   } catch (error) {

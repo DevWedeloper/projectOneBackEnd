19bb8f5f542dbc1f7049061b73d4c8c51b6ec34c
DevWedeloper <vicnathangabrielle@gmail.com>
2023-12-04 23:24:51 +0800

    Refactored characterModel with prettier

--- functions/src/models/characterModel.ts
+++ functions/src/models/characterModel.ts
@@ -16,30 +16,33 @@ export interface ICharacter {
 
 export type ICharacterWithoutId = Omit<ICharacter, '_id'>;
 
-const characterSchema = new Schema<ICharacterWithoutId>(
-  {
-    name: {
-      type: String,
-      required: true,
-      unique: true,
-      minlength: 6,
-      maxlength: 20,
-      match: /^[A-Za-z0-9_]*$/,
-      index: true,
-    },
-    characterType: { type: String, required: true },
-    health: { type: Number, required: true, min: 1000, max: 10000 },
-    strength: { type: Number, required: true, min: 1, max: 100 },
-    agility: { type: Number, required: true, min: 1, max: 100 },
-    intelligence: { type: Number, required: true, min: 1, max: 100 },
-    armor: { type: Number, required: true, min: 1, max: 100 },
-    critChance: { type: Number, required: true, min: 0.01, max: 1 },
-    guild: { type: Schema.Types.ObjectId, ref: 'Guild', default: null },
-  }
+const characterSchema = new Schema<ICharacterWithoutId>({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 6,
+    maxlength: 20,
+    match: /^[A-Za-z0-9_]*$/,
+    index: true,
+  },
+  characterType: { type: String, required: true },
+  health: { type: Number, required: true, min: 1000, max: 10000 },
+  strength: { type: Number, required: true, min: 1, max: 100 },
+  agility: { type: Number, required: true, min: 1, max: 100 },
+  intelligence: { type: Number, required: true, min: 1, max: 100 },
+  armor: { type: Number, required: true, min: 1, max: 100 },
+  critChance: { type: Number, required: true, min: 0.01, max: 1 },
+  guild: { type: Schema.Types.ObjectId, ref: 'Guild', default: null },
+});
+
+export const Character = model<ICharacterWithoutId>(
+  'Character',
+  characterSchema
 );
 
-export const Character = model<ICharacterWithoutId>('Character', characterSchema);
-
-export const create = async (character: ICharacterWithoutId): Promise<ICharacter> => {
+export const create = async (
+  character: ICharacterWithoutId
+): Promise<ICharacter> => {
   return (await Character.create(character)).toObject();
 };

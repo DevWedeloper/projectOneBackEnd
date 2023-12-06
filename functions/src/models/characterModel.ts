bd70b2a2d86af8cba94fe63869b8a7288083dc5f
DevWedeloper <vicnathangabrielle@gmail.com>
2023-12-06 16:20:24 +0800

    Separated schemas to another file, adjusted code accordingly

--- functions/src/models/characterModel.ts
+++ functions/src/models/characterModel.ts
@@ -1,36 +1,10 @@
 import {
   Document as MongooseDocument,
-  Schema,
-  UpdateWriteOpResult,
-  model,
+  UpdateWriteOpResult
 } from 'mongoose';
 import { ICharacter, ICharacterWithoutId } from '../types/characterTypes';
 import * as Guild from './guildModel';
-
-const characterSchema = new Schema<ICharacterWithoutId>({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-    minlength: 6,
-    maxlength: 20,
-    match: /^[A-Za-z0-9_]*$/,
-    index: true,
-  },
-  characterType: { type: String, required: true },
-  health: { type: Number, required: true, min: 1000, max: 10000 },
-  strength: { type: Number, required: true, min: 1, max: 100 },
-  agility: { type: Number, required: true, min: 1, max: 100 },
-  intelligence: { type: Number, required: true, min: 1, max: 100 },
-  armor: { type: Number, required: true, min: 1, max: 100 },
-  critChance: { type: Number, required: true, min: 0.01, max: 1 },
-  guild: { type: Schema.Types.ObjectId, ref: 'Guild', default: null },
-});
-
-const Character = model<ICharacterWithoutId>(
-  'Character',
-  characterSchema
-);
+import { Character } from './schemas/characterSchema';
 
 export const create = async (
   character: ICharacterWithoutId
--- functions/src/models/guildModel.ts
+++ functions/src/models/guildModel.ts
@@ -1,36 +1,8 @@
-import { Document as MongooseDocument, Schema, model } from 'mongoose';
+import { Document as MongooseDocument } from 'mongoose';
 import { ICharacter } from '../types/characterTypes';
 import { IGuild, IGuildWithoutId } from '../types/guildTypes';
 import * as Character from './characterModel';
-
-const guildSchema = new Schema<IGuildWithoutId>({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-    minlength: 6,
-    maxlength: 20,
-    match: /^[A-Za-z0-9_]*$/,
-    index: true,
-  },
-  leader: {
-    type: Schema.Types.ObjectId,
-    ref: 'Character',
-    required: true,
-    unique: true,
-  },
-  members: [{ type: Schema.Types.ObjectId, ref: 'Character' }],
-  totalMembers: { type: Number, default: 1, min: 1, max: 50 },
-  maxMembers: { type: Number, default: 50 },
-  totalHealth: { type: Number, default: 0 },
-  totalStrength: { type: Number, default: 0 },
-  totalAgility: { type: Number, default: 0 },
-  totalIntelligence: { type: Number, default: 0 },
-  totalArmor: { type: Number, default: 0 },
-  totalCritChance: { type: Number, default: 0 },
-});
-
-const Guild = model<IGuildWithoutId>('Guild', guildSchema);
+import { Guild } from './schemas/guildSchema';
 
 export const create = async (guild: IGuildWithoutId): Promise<IGuild> => {
   return (await Guild.create(guild)).toObject();

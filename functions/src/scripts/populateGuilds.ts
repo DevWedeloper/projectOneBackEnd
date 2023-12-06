ca9dc2bd61cc136bccb0e06761123d2265d4a352
DevWedeloper <vicnathangabrielle@gmail.com>
2023-12-06 17:56:09 +0800

    Fixed wrong import error

--- functions/src/scripts/populateGuilds.ts
+++ functions/src/scripts/populateGuilds.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import mongoose, { connect } from 'mongoose';
 import * as Character from '../models/characterModel';
 import * as Guild from '../models/guildModel';
-import { IGuild } from '../models/guildModel';
+import { IGuild } from '../types/guildTypes';
 import { joinGuild } from '../utils/guildCharacterUtils';
 dotenv.config({ path: '../../.env' });
 

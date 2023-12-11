edb4f62d7e2972e8d5099a6f12cb346e1aa0ffd8
DevWedeloper <vicnathangabrielle@gmail.com>
2023-12-11 22:29:15 +0800

    Refactored refreshAccessTokenType by adding without id type, used that in model

--- functions/src/auth/models/refreshAccessTokenModel.ts
+++ functions/src/auth/models/refreshAccessTokenModel.ts
@@ -1,11 +1,11 @@
 import { Schema, model } from 'mongoose';
-import { IRefreshAccessToken } from '../types/refreshAccessTokenType';
+import { IRefreshAccessTokenWithoutId } from '../types/refreshAccessTokenType';
 
-const RefreshTokenSchema: Schema<IRefreshAccessToken> = new Schema({
+const RefreshTokenSchema: Schema<IRefreshAccessTokenWithoutId> = new Schema({
   userId: { type: String, required: true },
   username: { type: String, required: true },
   token: { type: String, required: true },
   expiresAt: { type: Date, required: true },
 });
 
-export const RefreshToken = model<IRefreshAccessToken>('RefreshToken', RefreshTokenSchema);
+export const RefreshToken = model<IRefreshAccessTokenWithoutId>('RefreshToken', RefreshTokenSchema);

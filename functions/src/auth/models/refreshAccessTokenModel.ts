6e520e750b27e5edc1352511f8bba3e9b8772aba
DevWedeloper <vicnathangabrielle@gmail.com>
2023-12-11 23:02:37 +0800

    No longer directly expose schema, made a separate file for it

--- functions/src/auth/models/refreshAccessTokenModel.ts
+++ functions/src/auth/models/refreshAccessTokenModel.ts
@@ -1,20 +1,8 @@
-import { Schema, model } from 'mongoose';
 import {
   IRefreshAccessToken,
   IRefreshAccessTokenWithoutId,
 } from '../types/refreshAccessTokenType';
-
-const RefreshTokenSchema: Schema<IRefreshAccessTokenWithoutId> = new Schema({
-  userId: { type: String, required: true },
-  username: { type: String, required: true },
-  token: { type: String, required: true },
-  expiresAt: { type: Date, required: true },
-});
-
-export const RefreshToken = model<IRefreshAccessTokenWithoutId>(
-  'RefreshToken',
-  RefreshTokenSchema
-);
+import { RefreshToken } from './schemas/refreshAccessTokenSchema';
 
 export const create = async (
   token: IRefreshAccessTokenWithoutId

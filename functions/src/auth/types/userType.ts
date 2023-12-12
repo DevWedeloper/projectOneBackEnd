6539a0e4ce47588a2df09199e9482e84108e6b33
DevWedeloper <vicnathangabrielle@gmail.com>
2023-12-12 17:49:29 +0800

    Removed auth related code

--- functions/src/auth/types/userType.ts
+++
@@ -1,8 +0,0 @@
-export type IUser = {
-  _id: string;
-  username: string;
-  password: string;
-  role: 'admin' | 'standard';
-};
-
-export type IUserWithoutId = Omit<IUser, '_id'>;
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,10 @@
 import express, { Application } from 'express';
 import { connect } from 'mongoose';
 import * as functions from 'firebase-functions';
-// import cron from "node-cron";
 import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
-// import { refreshTokenCleanup } from "./cron/refreshTokenCleanup";
-import userRouter from './auth/routes/userRoute';
-import loginRoute from './auth/routes/loginRoute';
 import characterRoute from './routes/characterRoute';
 import guildRoute from './routes/guildRoute';
 import characterStatsRoute from './routes/characterStatsRoute';
@@ -52,8 +48,6 @@ const connectToDatabase = async () => {
 // connectToDatabase();
 app.use(express.json());
 
-app.use('/user', userRouter);
-app.use('/', loginRoute);
 app.use('/character', characterRoute);
 app.use('/guild', guildRoute);
 app.use('/characterStats', characterStatsRoute);
@@ -67,6 +61,5 @@ app.use('/', isGuildFull);
 // app.listen(process.env.MY_PORT, () => {
 //   console.log(`Server is running on PORT ${process.env.MY_PORT}`);
 // });
-// cron.schedule('0 0 * * *', refreshTokenCleanup);
 
 export const api = functions.https.onRequest((connectToDatabase(), app));
